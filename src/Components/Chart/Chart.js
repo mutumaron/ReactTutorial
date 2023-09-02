edf2ef5aef990b72078d862a5b2e243c8f8cf8bc
mutumaron <mutembeironny11@gmail.com>
2023-09-02 16:53:19 +0300

    finished project

---
+++ src/Components/Chart/Chart.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import "./Chart.css";
+import ChartBar from "./ChartBar";
+
+const Chart = (props) => {
+  const dataPointValues = props.dataPoints.map((dataPoint) => {
+    return dataPoint.value;
+  });
+  const totalMaxValue = Math.max(...dataPointValues);
+
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={totalMaxValue}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Chart;
--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,49 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 // import ExpenseItem from './ExpenseItem';
-import ExpensesFilter from './ExpensesFilter';
-import './Expenses.css';
-import Card from '../UI/Card';
-import ExpenseList from './ExpenseList';
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
+import Card from "../UI/Card";
+import ExpenseList from "./ExpenseList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState("2020");
 
+  //   const filterChangeHandler = selectedYear => {
+  //     setFilteredYear(selectedYear);
+  //   };
 
-//   const filterChangeHandler = selectedYear => {
-//     setFilteredYear(selectedYear);
-//   };
+  // let filterInfoText = "2021,2022 & 2023";
 
-    // let filterInfoText = "2021,2022 & 2023";
+  // if (filteredYear === "2020"){
+  //     filterInfoText = "2021,2022 & 2023";
+  // }else if(filteredYear === "2021"){
+  //     filterInfoText = "2020,2022 & 2023";
+  // }else if(filteredYear === "2022"){
+  //     filterInfoText = "2020,2021 & 2023";
+  // }else{
+  //     filterInfoText = "2020,2021 & 2022";
+  // }
 
-    // if (filteredYear === "2020"){
-    //     filterInfoText = "2021,2022 & 2023";
-    // }else if(filteredYear === "2021"){
-    //     filterInfoText = "2020,2022 & 2023";
-    // }else if(filteredYear === "2022"){
-    //     filterInfoText = "2020,2021 & 2023";
-    // }else{
-    //     filterInfoText = "2020,2021 & 2022";
-    // }
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
-
-    const filterChangeHandler = (selectedYear) => {
-        setFilteredYear(selectedYear);
-    };
-
-    const filteredExpenses = props.items.filter((expense) => {
-        return expense.date.getFullYear().toString() === filteredYear;
-    });
-
-    
   return (
     <div>
-      <Card className='expenses'>
-        <ExpensesFilter selected ={filteredYear} onChangeFilter = {filterChangeHandler}/>
-        <ExpenseList items = {filteredExpenses} />
-       
-   
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpenseList items={filteredExpenses} />
       </Card>
     </div>
   );
